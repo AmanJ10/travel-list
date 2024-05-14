f17e99a45e95b6ace967abc44ae6a2f2fededaeb
AmanJ10 <joharapurkaraman95@gmail.com>
2024-05-14 19:31:32 +0530

    Completed

---
+++ src/components/App.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import Logo from "./Logo.js";
+import Form from "./Form.js";
+import PackingList from "./PackingList.js";
+import Stats from "./Stats.js";
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItem(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((items) => items.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+  function handleClear() {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+    if (confirmed) setItems([]);
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItem} />
+      <PackingList
+        items={items}
+        onClearItem={handleClear}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
---
+++ src/components/Form.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+export default function Form({ onAddItems }) {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(5);
+
+  function handleSubmit(e) {
+    e.preventDefault(); // This doesnt reload the web page
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+    onAddItems(newItem);
+
+    // setting fields back to its initial states.
+    setDescription("");
+    setQuantity(1);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => {
+          // console.log(e.target.value);
+          setQuantity(Number(e.target.value));
+        }}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => {
+          // console.log(e.target.value);
+          setDescription(e.target.value);
+        }}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
